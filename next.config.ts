3a08d63d697a73406d5616a472cd02fd215db92b
imajijul <imdajijul740@gmail.com>
2025-11-30 02:02:16 +0600

    feature img changed

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["www.stimulsoft.com"],
-  },
+ 
 };
 
 export default nextConfig;
